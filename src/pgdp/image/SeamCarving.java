b48e6568626d7e27c60498de21a2f1a73bfaf637
Robin Marchart <robin.marchart@tum.de>
2022-09-21 16:07:52 +0200

    Exercise-Template pushed by Artemis

---
+++ src/pgdp/image/SeamCarving.java
@@ -0,0 +1,29 @@
+package pgdp.image;
+
+import java.util.Arrays;
+
+public class SeamCarving {
+
+	public int computeGradientMagnitude(int v1, int v2) {
+		return -1;
+	}
+
+	public void toGradientMagnitude(int[] picture, int[] gradientMagnitude, int width, int height) {
+	}
+
+	public void combineMagnitudeWithMask(int[] gradientMagnitude, int[] mask, int width, int height){
+
+	}
+
+	public void buildSeams(int[][] seams, long[] seamWeights, int[] gradientMagnitude,
+			int width, int height) {
+	}
+
+	public void removeSeam(int[] seam, int[] image, int height, int oldWidth) {
+	}
+
+	public int[] shrink(int[] image,int[] mask, int width, int height, int newWidth) {
+		return null;
+	}
+
+}
